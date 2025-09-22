93d4f179a3b44c64483ea39d486fbcab45ecf2d5
Chechel01 <m1chellin4869@gmail.com>
2025-09-22 00:42:29 -0300

    Add files via upload

---
+++ gerar-qrcode-main/js/script.js
@@ -0,0 +1,27 @@
+const url = document.querySelector('#url');
+const botao = document.querySelector('#botao');
+const qr = document.querySelector('#qr');
+
+botao.addEventListener('click',gerador);
+url.addEventListener('keydown', (event) =>{
+    if(event.key === 'Enter'){
+        gerador();
+    }
+});
+
+function gerador(){
+    texto = url.value
+    if(texto){
+        qr.innerHTML = ''
+        code = new QRCode(qr,{
+            text: texto,
+            width: 400,
+            height: 400,
+            colorDark: 'rgba(0,0,0,0)',
+            colorLight: 'black'
+        }
+        )
+    }else{
+        qr.innerHTML = ''
+    }
+}
